34c007bdb8c073fa6d8311412378ba67c7106c4d
desaw <desaw@AZAMIND>
2022-04-25 15:55:48 +0300

    Add project files.

---
+++ XamarinLearnApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinLearnApp.ViewModels;
+
+namespace XamarinLearnApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
